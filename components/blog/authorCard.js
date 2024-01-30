53cb3c1449040b20ba32fc335770b0b901ec98c1
semih babacan huawei <m.semihbabacan@gmail.com>
2024-01-30 13:14:19 +0300

    Update: updaetd authorCrad

--- components/blog/authorCard.js
+++ components/blog/authorCard.js
@@ -24,7 +24,7 @@ export default function AuthorCard({ author }) {
         <div>
           <div className="mb-3">
             <h3 className="text-lg font-medium text-gray-800 dark:text-gray-300">
-              About {author.name}
+              {author.name} Hakkında
             </h3>
           </div>
           <div>
