77c5aa90dbce5141b0a590f778a3e645a9b09825
semih babacan huawei <m.semihbabacan@gmail.com>
2024-02-09 12:14:21 +0300

    Update: updated the img urls

--- app/(website)/faaliyetlerimiz/aile/page.js
+++ app/(website)/faaliyetlerimiz/aile/page.js
@@ -11,37 +11,37 @@ export default async function AboutPage() {
 
 const ailelerSectionsData =  [
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "ANNE VE ÇOCUK ATÖLYESİ",
     paragraph: "Anne ve çocuk bağını güçlendirmek amacıyla düzenlenen atölyeler, eşsiz bir deneyim sunar. Bu atölyeler, annelerle çocukları arasındaki duygusal bağı pekiştirmenin yanı sıra, birlikte geçirilen kaliteli zamanın değerini vurgular. Anne ve çocuk birlikte faydalı etkinliklere katılır. El sanatları, oyunlar, hikâye anlatımı gibi etkinliklerle anne ve çocuk arasında güçlü bir etkileşim sağlanır. Aynı zamanda, paylaşılan deneyimler sayesinde anlayış ve empati gelişir. Bu atölyeler, annelerin çocuklarıyla daha derinlemesine iletişim kurmalarına olanak tanır ve çocukların duygusal gelişimine olumlu bir katkı sağlar."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "MANEVİ EĞİTİM ATÖLYESİ",
     paragraph: "Bu atölye, katılımcıları İslami ve Kurani hakikatler ışığında manevi bir yolculuğa çıkararak ruhsal derinlik kazanmaya ve kişisel gelişimlerine islamî değerlerle rehberlik etmeyi amaçlar. İslamın manevi zenginlikleri olan Kuran-ı Kerim, tefsir, ahlak, hanım sahabeler ve siyer dersleri ile İslamın değerlerini ve manevi deneyimini derinleştirmek isteyen bireyler için etkileşime dayalı bir ortam sunar."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "KENDİNİ TANIMA ATÖLYESİ",
     paragraph: "Kişisel gelişim ve farkındalık üzerine odaklanan etkinliklerdir. Bu atölyelere katılarak kendi değerlerinizi, güçlü yönlerinizi ve hedeflerinizi daha iyi gözlemleyebilirsiniz. Öz farkındalık çalışmaları, duygusal zeka becerileri, etkili iletişim ve manevi rehberlik konularında size yol gösterecek çalışmalar yapılmaktadır. Grup içinde, rahat bir atmosferde deneyim paylaşımı ve interaktif uygulamalarla samimi bir öğrenme ortamı sunar."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "İLMİ ÇALIŞMALAR VE YAZIM ATÖLYESİ",
     paragraph: "Bu atölye, katılımcılara manevi ilmi ve bilimsel araştırmalar yapma ve bu araştırmaları etkili bir şekilde yazma konusunda beceriler kazandırmayı hedefler."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "SANAT VE MUSİKİ",
     paragraph: "İslami Sanat, Allah'a olan derin sevgiyi ve ibadeti ifade eden manevi zenginliği yansıtarak bir köprü oluşturur. “Minyatür, Hilye-i Şerif, Tezhip ve Hat sanatı” bu sanatların aracılığı ile İslamın derin maneviyatını ve estetik anlayışını keşfetmeye dair bir alan sunar. Tasavvuf Musikisi, insanın duygu ve düşüncelerini melodiler, ritimler ve sesler yoluyla ifade etmesini sağlayan sanat dalıdır. Bu müzik, İslam Tasavvuf geleneğinin ruhsal ve mistik boyutlarını ifade etmek amacıyla kullanılır. Bu atölyede, enstrümanlar arasında; ney, bendir ve kanun gibi eğitimler verilecektir."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "ZARAFET ATÖLYESİ",
     paragraph: "Zarafet atölyesi, annelerin kendilerini zarif ve özgüvenli hissetmelerine yönelik benzersiz bir deneyim sunar. Bu atölyeler; beden dili, duruş, zarif hareketler ve iletişim konularında eğitim içerir. İslami perspektifle Peygamberimizi model alarak toplumsal uyumluluğu ve toplumsal ilişkileri güçlendirmek ve kaliteli bireyler yetiştirmektir. Katılımcılar, profesyonel eğitmenler eşliğinde günlük yaşantıda zarafetlerini arttırmak için pratik bilgiler edinirler. Eğlenceli etkinlikler ve gerçek hayattan örneklerle desteklenen atölyelerde, anneler özgüvenlerini arttırmanın yanı sıra sosyal ortamlarda daha rahat ve zarif olma becerilerini geliştirirler."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "KÜLTÜR GEZİLERİ",
     paragraph: "Kültür gezileri, annelere hem dinlenme hem de öğrenme fırsatı sunan benzersiz bir deneyimdir. Bu gezilerde anneler; sanat, tarih, ve kültürel zenginlikleri keşfederken ayrıca islami kültürü anlamak için yapılan kültür gezileri, tarihi islam eserlerini ziyaret etmek, islam mimarisini incelemek ve islam sanatını keşfetmek gibi aktiviteler içerir. Bu geziler, islam tarihini, değerlerini ve toplumsal dinamiklerini daha derinlemesine anlama fırsatı sunar. Bu tür aktiviteler, anneler arasında sosyal bağları güçlendirecek ve keyifli bir öğrenme ortamı oluşturacaktır."
   }
--- app/(website)/faaliyetlerimiz/cocuk/page.js
+++ app/(website)/faaliyetlerimiz/cocuk/page.js
@@ -4,27 +4,27 @@ import FaaliyetlerCommon from "../../../../components/faaliyetlerCommon";
 
 const cocuklarSectionsData = [
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "MANEVİ DEĞERLER ATÖLYESİ",
     paragraph: "Manevi değerler atölyesi, çocuklara karakter gelişimini destekleyen ahlaki ve dini etkinlikler sunan bir ortamdır. Bu atölyeler, manevi değerleri öğrenmeye yönelik, eğlenceli aktiviteler içerir."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "SANAT VE MUSİKİ ATÖLYESİ",
     paragraph: "Çocuklar için sanat atölyesi, üretkenlik becerilerini geliştirmelerine ve ifade becerilerini keşfetmelerine olanak sağlayan bir ortamdır. Bu atölyeler, çeşitli sanat teknikleriyle tanışma, özgün eserler oluşturma ve duygusal ifadeyi sanat aracılığıyla paylaşma konularında çocukları destekler. Çocukları duygusal bir deneyime davet eden ve müziği manevi bir bağlamda kullanarak, kişisel gelişimi destekleyen bir etkinlik alanıdır. Bu atölyede, enstrümanlar arasında; ney, bendir ve kanun gibi eğitimler verilecektir."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "YAŞAM BECERİLERİ",
     paragraph: "Çocukların günlük yaşamlarında başarılı olabilmek ve kişisel gelişimlerini destekleyebilmek için, ihtiyaç duydukları pratik yetenekleri ve bilgileri kapsar. Bu beceriler çocuğun bağımsızlık, özgüven ve genel yaşam kalitesini artırmaya yöneliktir."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "DUYGU VE DRAMA ATÖLYESİ",
     paragraph: "Duygu ve drama atölyeleri, çocuklara hissetmeyi, anlamayı ve paylaşmayı öğreten bir sanat formudur. Atölye deneyimleri, çocukların duygusal derinliklerini keşfetmelerini ve kendi hikayelerini oluşturmalarını sağlar. Bu atölyeler, çocukların empati, dayanışma ve manevi güçlerini bulmalarına katkıda bulunur."
   },
   {
-    imgUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
+    imageUrl: "/pictures/faaliyetlerimiz/aile/family.jpg",
     title: "TEKNOLOJİ VE BİLİM ATÖLYESİ",
     paragraph: "Çocuklar için bilim ve teknoloji, meraklarını canlı tutarak, keşif yapma arzusunu teşvik eden bir öğrenme sürecidir. Bu süreç, çocuklara gözlem yapma, deneyler gerçekleştirme ve teknolojik araçları anlama fırsatı sunarak bilimsel düşünme ve problem çözme yeteneklerini geliştirmelerine yardımcı olur. Aynı zamanda, eğlenceli ve etkileşimli bir ortamda, bilim ve teknolojiyi anlamanın keyfini çocuklara sunar."
   }
